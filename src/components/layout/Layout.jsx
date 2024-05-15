50e5b743f2ccaff1e0798745bb22da3b799d0116
Selene <selenedanyluk@gmail.com>
2024-05-14 21:24:01 -0300

    layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,14 +4,16 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Layout.css'
 
 const Layout = () => {
     return (
         <div>
-            <Navbar expand="lg" className="bg-body-tertiary">
+            <Container fluid className='navbar-container-1'>
+                Logo
+            </Container>
+            <Navbar expand="lg" className="navbar-container-2">
                 <Container fluid>
-                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
@@ -19,30 +21,19 @@ const Layout = () => {
                             style={{ maxHeight: '100px' }}
                             navbarScroll
                         >
-                            <Nav.Link href="#action1">Home</Nav.Link>
-                            <Nav.Link href="#action2">Link</Nav.Link>
-                            <NavDropdown title="Link" id="navbarScrollingDropdown">
-                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action4">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action5">
-                                    Something else here
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link href="#" disabled>
-                                Link
-                            </Nav.Link>
+                            <Nav.Link className='beige-claro' href="#action1">Nosotros</Nav.Link>
+                            <Nav.Link className='beige-claro' href="#action2">Libros</Nav.Link>
+                            <Nav.Link className='beige-claro' href=''>Autoras</Nav.Link>
+                            <Nav.Link className='beige-claro' href="#">Contacto</Nav.Link>
                         </Nav>
                         <Form className="d-flex">
                             <Form.Control
                                 type="search"
-                                placeholder="Search"
+                                placeholder="Buscar"
                                 className="me-2"
                                 aria-label="Search"
                             />
-                            <Button variant="outline-success">Search</Button>
+                            <Button variant="outline-secondary search-button">Buscar</Button>
                         </Form>
                     </Navbar.Collapse>
                 </Container>
